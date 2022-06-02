3c16e303eac958ef280ac02beaa7901e21522f07
Cristian Farias Verdugo <krismarrano@hotmail.com>
2022-06-02 00:41:49 -0400

    subida proyecto node

---
+++ src/express-config/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import routes from '../routes'
+
+const app = express()
+
+app.disable('x-powered-by')
+app.use(express.urlencoded({limit: '50mb', extended: true}))
+app.use(express.json({limit: '50mb'}))
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.CROSS_ORIGIN || '*')
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+    next()
+})
+
+
+
+app.use(routes)
+
+export default app
